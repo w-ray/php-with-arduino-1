bc4cace2adc5166c38dbc17902298e5ee2dec712
w_ray <xd_nam@hotmail.com>
2014-09-23 10:56:05 +0800

    Update add.php

--- add.php
+++ add.php
@@ -1,8 +1,14 @@
 <?php
     include ("connect.php");
-	$link = Con();
+	$link = connection();
 	$new_temp = new_temperature;
 	$new_humid = new_humidity;
 	$old_temp = old_temperature;
 	$old_humid = old_humidity;
+	$client.print $new_temp;
+	$client.print $new_humid;
+	$client.print $old_temp;
+	$client.print $old_humid;
+	
+	
 ?>
